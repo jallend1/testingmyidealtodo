78c7f4d710f59c833a4083b5ae594ef2a80555e8
jallend1 <jallend1@gmail.com>
2021-01-02 15:22:00 -0800

    Only superficial changes, but want to make sure it matches when I start working from another station

--- src/Components/List.js
+++ src/Components/List.js
@@ -7,8 +7,10 @@ const List = ({list}) => {
       <h3 key={list.id}>{list.details.title}</h3>
       <h4>By {list.details.author}</h4>
       {list.details.contents.map((content, index) => (
-        <p key={list.id + (index * .1)}>{content}</p>
-      ))}
+        <p key={list.id + (index * .1)}>
+          {content}
+        </p>
+        ))}
       <NewTask />
     </>
   );
