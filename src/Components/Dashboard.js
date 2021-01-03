5e067a623a300718e097a88f1cfff2a41c707c63
jallend1 <jallend1@gmail.com>
2021-01-03 11:49:42 -0800

    Fixes infinite bug loop that had it constantly downloading Firestore data

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,21 +4,22 @@ import List from "./List";
 
 const Dashboard = () => {
   const [taskLists, setTaskLists] = useState([]);
-  
   //Pulls existing tasklists from Firebase and puts them in state
   const retrieveLists = () => {
     const taskListRef = firebase.firestore().collection('tasklists');
-    const todoLists = [];
-    taskListRef.get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(list => {
-        const taskList = {};
-        taskList.id = list.id;
-        taskList.details = list.data();
-        todoLists.push(taskList)
+    if(taskLists.length === 0){
+      const todoLists = [];
+      taskListRef.get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(list => {
+          const taskList = {};
+          taskList.id = list.id;
+          taskList.details = list.data();
+          todoLists.push(taskList)
+        });
+        setTaskLists(todoLists);
       });
-      setTaskLists(todoLists);
-    });
+    }
   }
   
   useEffect(retrieveLists, [taskLists, setTaskLists]);
